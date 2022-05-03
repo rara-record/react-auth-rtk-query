37a797f5c966ca4cebf26fbbfb37bd46aa9e9a97
bora <a52447879@gmail.com>
2022-05-03 19:23:22 +0900

    [feat] 라우터 설정 및 react-toolkit 패키지 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+import Home from "./pages/Home/Home";
+import Signin from "./pages/Signin/Signin";
+import Signup from "./pages/Signup/Signup";
+import NotFound from "./pages/NotFound/NotFound";
+import SendEmail from "./pages/SendEmail/SendEmail";
+import EmailVerify from "./pages/EmailVerify/EmailVerify";
+import ChangePassword from "./pages/ChangePassword/ChangePassword";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/send-verify-mail" element={<SendEmail />} />
+        <Route path="/email-verify/:token" element={<EmailVerify />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/forgot-password-verify/:token"
+          element={<ChangePassword />}
+        />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
