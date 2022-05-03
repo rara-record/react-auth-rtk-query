17bf5d69a1994024cd70108077cb36c680c26e97
bora <a52447879@gmail.com>
2022-05-03 19:49:13 +0900

    [feat] api 파일 생성, store에 연결

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+import { authApi } from "./api/authApi";
 import authReducer from "./state/authSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+setupListeners(store.dispatch);
