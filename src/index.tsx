b280df65075025187337fc95bb45c834f07e0803
kim bo ra <a52447879@gmail.com>
2022-05-04 12:40:04 +0900

    [feat] chakra UI 설치 및 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { ChakraProvider } from "@chakra-ui/react";
+
 import { store } from "./store/index";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </Provider>,
 
   document.getElementById("root")
