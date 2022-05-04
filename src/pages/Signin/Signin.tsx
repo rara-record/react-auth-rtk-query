79c2f226bf0c83f72d23e11b42ae4ea78caf7112
kim bo ra <a52447879@gmail.com>
2022-05-04 12:47:48 +0900

    [feat] formik 기본 구조 설정

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,5 +1,23 @@
+import { Form, Formik } from "formik";
+
 const Signin = () => {
-  return <div>Signin</div>;
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={(valuse) => {
+        console.log(valuse);
+      }}
+    >
+      <Form></Form>
+    </Formik>
+  );
 };
 
 export default Signin;
+
+/*
+ * formik의 구조
+    - <Formik> 컴포넌트 안에 initialValues와 onSubmit이 꼭 들어가야하는 props이다.
+    - 각각 초기 값과 폼 제출시에 실행될 함수이다
+
+ */
